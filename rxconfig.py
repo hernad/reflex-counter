e225f6c3d035544a3b4a72c754c167176a55d8bc
Ernad Husremovic <hernad@bring.out.ba>
2024-11-30 20:29:12 +0100

    sqlite:////

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex_counter",
-    db_url="sqlite:///var/lib/reflexcounter/reflex-counter.db",
+    db_url="sqlite:////var/lib/reflexcounter/reflex-counter.db",
     api_url="https://reflex-counter-backend.bring-out-ba.uk",
     env=rx.Env.PROD,
 )
