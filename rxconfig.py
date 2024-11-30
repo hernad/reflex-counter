3df8a805d9dcd34076b121fa5fdfce161ad11bdc
Ernad Husremovic <hernad@bring.out.ba>
2024-11-30 19:01:02 +0100

    app_name: reflex-counter

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="reflex_counter_backend",
+    app_name="reflex-counter",
     db_url="sqlite:///reflex-counter.db",
     api_url="https://reflex-counter-backend.bring-out-ba.uk",
     env=rx.Env.PROD,
