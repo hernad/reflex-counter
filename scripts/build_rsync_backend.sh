eb5bfbf5d225431f1a323947abfaf5b7ce160d02
Ernad Husremovic <hernad@bring.out.ba>
2024-11-30 19:12:18 +0100

    app_name=reflex_counter

--- scripts/build_rsync_backend.sh
+++ scripts/build_rsync_backend.sh
@@ -2,6 +2,7 @@
 
 rm -rf backend
 rm reflex-counter-backend.zip
+rm reflex_counter_backend.zip
 reflex_counter export --backend-only
 mkdir backend
 cd backend
@@ -9,9 +10,9 @@ unzip ../backend.zip
 rm -rf scripts
 cd ..
 
-zip -r reflex-counter-backend.zip backend
+zip -r reflex_counter_backend.zip backend
 rm -rf backend
 
 
-rsync -avz reflex-counter-backend.zip root@download.svc.bring.out.ba:/data/download/
+rsync -avz reflex_counter_backend.zip root@download.svc.bring.out.ba:/data/download/
 
